d43c4e95aa45132b52e78ac721e5bc288885715f
Juan Gerardo Madero Flores <juan@Juans-MacBook.attlocal.net>
2020-04-13 14:05:14 -0700

    dummy reducer and initial state

---
+++ reducer-todo/src/reducers/listReducer.js
@@ -0,0 +1,14 @@
+export const listReducer = (state, action) => {
+    switch(action.type){
+        default:
+            return state
+    }
+}
+
+export const initialState = [
+    {
+        item: 'Learn about reducers',
+        completed: false,
+        id: 3892987589,
+    }
+]
